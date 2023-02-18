dadf283da7a53c3dbd15ca840cac226d15fbe6e8
basu1020 <bvp1020mu@gmail.com>
2023-02-18 12:00:49 +0530

    moved backend files into a seperate file

--- Book-A-Movie-Ticket-Fullstack-Boilerplate-main/src/backend/connector.js
+++ Book-A-Movie-Ticket-Fullstack-Boilerplate-main/src/backend/connector.js
@@ -4,7 +4,7 @@ const mongodb = require('mongodb');
 const mongoURI = process.env.MONGO_URI 
 
 let mongoose = require('mongoose');
-const { bookMovieSchema } = require('./schema')
+const { bookMovieSchema } = require('../schema')
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => { console.log("connection established with mongodb server online"); })
